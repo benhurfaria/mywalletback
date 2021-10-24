107276e89b3d2e8adef8b2f39788b924471e4158
Ben Hur Faria <benhurfaria3@gmail.com>
2021-10-24 14:25:07 -0300

    post signup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import pg from 'pg';
+import {v4 as uuid} from 'uuid';
+import bcrypt from 'bcrypt';
+
 
 const app = express();
 app.use(cors());
@@ -9,11 +12,42 @@ app.use(express.json());
 const {Pool} = pg;
 
 const connection = new Pool({
-    user: 'bootcamp_role',
+    user: 'postgres',
     password: '123456',
     host: 'localhost',
     port: 5432,
-    database: 'boardcamp'
+    database: 'mywallet'
+});
+
+app.post("/signup", async (req,res)=>{
+    const {
+        nome, 
+        email,
+        password
+    } = req.body;
+   
+    try{
+        const hash = bcrypt.hashSync(password,10);
+        const result = await connection.query(`
+            SELECT * FROM usuarios WHERE email = $1;
+        `, [email]);
+
+        
+        if(result.rowCount >= 1){
+            return res.sendStatus(400);
+            
+        }
+        console.log(email);
+        await connection.query(`
+            INSERT INTO usuarios (nome, email, password) VALUES ($1, $2, $3);
+        `, [nome, email, hash]);
+
+        return res.sendStatus(200);
+        
+    } catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }
 });
 
 app.listen(4000);
